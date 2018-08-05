82dc6ed391ad468919c11681daa2c575fa9a54ea
Dmitriy Savin <its2exp@gmail.com>
2018-08-05 17:57:10 +0300

    Add tests for app component

---
+++ test/javascript/app.test.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as renderer from 'react-test-renderer'
+
+import { default as App } from 'Javascript/app'
+
+it('renders app component with a custom name', () => {
+    const component = renderer.create(<App name={'Dmitriy'} />)
+    expect(component).toMatchSnapshot()
+})
+
+it('renders app component with a default name', () => {
+    const component = renderer.create(<App />)
+    expect(component).toMatchSnapshot()
+})
